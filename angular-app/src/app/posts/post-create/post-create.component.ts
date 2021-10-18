16d1b93d7922c7906a440ebb096f3e81048b51e0
Lev <lev.barsegyan@devit-team.com>
2021-10-18 08:35:09 +0000

    Basic post function working on the frontend

--- angular-app/src/app/posts/post-create/post-create.component.ts
+++ angular-app/src/app/posts/post-create/post-create.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
+import { format } from 'url';
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = '';
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  title = "";
+  description = "";
+  constructor(public postService: PostService) { }
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.title = form.value.enteredTitle;
+    this.description = form.value.enteredDescription;
+    this.postService.addPost(this.title, this.description);
   }
 }
